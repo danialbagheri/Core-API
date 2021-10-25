3107546310c35744af23b00c1ce94c12956b1ffe
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2021-10-25 12:54:55 +0330

    Add more info in user serializer

--- calypso/user/api/serializers/user.py
+++ calypso/user/api/serializers/user.py
@@ -1,15 +1,22 @@
+from django.core.exceptions import ValidationError
 from rest_framework import serializers
 
 from user.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    re_password = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
         fields = (
             'id',
+            'first_name',
+            'last_name',
             'email',
             'password',
+            're_password',
         )
         read_only_fields = (
             'id',
@@ -17,6 +24,9 @@ class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         password = validated_data.pop('password')
+        re_password = validated_data.pop('re_password')
+        if password != re_password:
+            raise ValidationError('Invalid Password Repeat.')
         new_user = super().create(validated_data)
         new_user.set_password(password)
         new_user.save()
