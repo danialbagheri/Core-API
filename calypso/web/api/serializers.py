467e28d41c2c955d161687d4b5ff2dd258ee91bf
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2021-11-01 14:38:52 +0330

    Fix 500 errors on contact us API

--- calypso/web/api/serializers.py
+++ calypso/web/api/serializers.py
@@ -26,17 +26,26 @@ class ContactFormSerializer(serializers.Serializer):
     reason = serializers.ChoiceField(choices=REASON_CHOICES,)
     message = serializers.CharField()
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.recaptcha_value = None
+
     def to_internal_value(self, data):
-        if 'recaptcha' not in data:
+        self.recaptcha_value = data.get('recaptcha')
+        return super().to_internal_value(data)
+
+    def validate(self, attrs):
+        if not self.recaptcha_value:
             raise ValidationError('Recaptcha data not sent.')
+
         ip, _ = get_client_ip(self.context['request'])
         response = requests.post(
             url=f'https://www.google.com/recaptcha/api/siteverify?'
-                f'secret={settings.DRF_RECAPTCHA_SECRET_KEY}&response={data["recaptcha"]}&remoteip={ip}',
+                f'secret={settings.DRF_RECAPTCHA_SECRET_KEY}&response={self.recaptcha_value}&remoteip={ip}',
         )
         if response.status_code != 200 or not response.json().get('success', False):
-            raise ValidationError('Recaptcha validation failed')
-        return super().to_internal_value(data)
+            raise ValidationError('Recaptcha validation failed.')
+        return attrs
 
 
 class SlideSerializer(serializers.ModelSerializer):
