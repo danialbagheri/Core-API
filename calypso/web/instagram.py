dffc4d1d972246983ec8d554458b360a9d628803
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2021-12-13 15:21:04 +0330

    Fix instagram connection

--- calypso/web/instagram.py
+++ calypso/web/instagram.py
@@ -1,20 +1,40 @@
-from django.conf import settings
+from datetime import datetime, timedelta
+
 import requests
+from django.conf import settings
 
-# To gain long lived instant token, I have registered with https://www.instant-tokens.com/
-instance_token_api_url = settings.INSTAGRAM_INSTANT_TOKEN_API
+from web.models import Configuration
 
 
-def get_instagram_token():
-    r = requests.get(instance_token_api_url)
-    token = r.json()['Token']
-    return token
+def refresh_instagram_access_token(access_token):
+    url = f'https://graph.instagram.com/refresh_access_token?' \
+          f'grant_type=ig_refresh_token&' \
+          f'access_token={access_token}'
+    response = requests.get(url)
+    new_token = response.json()['access_token']
+    Configuration.objects.filter(key='instagram_access_token').update(value=new_token)
+    Configuration.objects.filter(
+        key='instagram_access_token_timestamp'
+    ).update(value=datetime.now().strftime('%Y-%m-%d'))
+    return response.json()['access_token']
 
 
 def get_user_feed():
-    access_token = get_instagram_token()
+    token_config = Configuration.objects.filter(key='instagram_access_token').first()
+    if not token_config:
+        return []
+    access_token = token_config.value
+    access_token_timestamp = Configuration.objects.filter(key='instagram_access_token_timestamp').first()
+    if (
+        access_token_timestamp and
+        datetime.now() - datetime.strptime(access_token_timestamp.value, '%Y-%m-%d') > timedelta(days=30)
+    ):
+        access_token = refresh_instagram_access_token(access_token)
+
     instagram_user_id = settings.INSTAGRAM_USER_ID
-    instagram_media_feed_url = f"https://graph.instagram.com/{instagram_user_id}/media?fields=media_url,caption,permalink,media_type&access_token={access_token}"
+    instagram_media_feed_url = f"https://graph.instagram.com/{instagram_user_id}/media?" \
+                               f"fields=media_url,caption,permalink,media_type&" \
+                               f"access_token={access_token}"
     r = requests.get(instagram_media_feed_url)
     response = r.json()['data']
     return response
