67e8b29d245229d6d7650c08ac723d0b86f7a413
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2022-08-01 19:13:43 +0430

    Add some fields to review image admin

--- calypso/review/admin/review_image.py
+++ calypso/review/admin/review_image.py
@@ -5,4 +5,11 @@ from review.models import ReviewImage
 
 @admin.register(ReviewImage)
 class ReviewImageAdmin(admin.ModelAdmin):
+    list_display = ['id', 'get_review_title']
+    search_fields = ('review__title',)
     readonly_fields = ('review',)
+
+    def get_review_title(self, review_image: ReviewImage):
+        return review_image.review.title
+
+    get_review_title.short_description = 'Review Title'
