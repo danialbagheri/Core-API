0e185b66afba0564a1f6f5b2c201caa714410a35
Ruhollah Sekaleshfar <r.sekaleshfar@fastbundle.co>
2023-03-06 02:33:27 +0330

    Add image_type and image_angle to variant image request model

--- calypso/user/models/variant_image_request.py
+++ calypso/user/models/variant_image_request.py
@@ -2,9 +2,55 @@ from django.db import models
 
 
 class VariantImageRequest(models.Model):
+    TYPE_PRODUCT_IMAGE = 'PI'
+    TYPE_LIFE_STYLE = 'LS'
+    TYPE_RANGE_PHOTO = 'RP'
+    TYPE_OTHERS = 'OT'
+    TYPE_ALL = 'ALL'
+    IMAGE_TYPE_CHOICES = (
+        (TYPE_PRODUCT_IMAGE, 'Product Image'),
+        (TYPE_LIFE_STYLE, 'Life Style'),
+        (TYPE_RANGE_PHOTO, 'Range Photo'),
+        (TYPE_OTHERS, 'Others'),
+        (TYPE_ALL, 'All'),
+    )
+
+    ANGLE_FRONT = 'FRONT'
+    ANGLE_BACK = 'BACK'
+    ANGLE_ANGLE = 'ANGLE'
+    ANGLE_TOP = 'TOP'
+    ANGLE_RIGHT_SIDE = 'RIGHT_SIDE'
+    ANGLE_LEFT_SIDE = 'LEFT_SIDE'
+    ANGLE_BOTTOM = 'BOTTOM'
+    ANGLE_CUSTOM = 'CUSTOM'
+    ANGLE_ALL = 'ALL'
+    IMAGE_ANGLE_CHOICES = (
+        (ANGLE_FRONT, 'Front'),
+        (ANGLE_BACK, 'Back'),
+        (ANGLE_ANGLE, 'Angle'),
+        (ANGLE_TOP, 'Top'),
+        (ANGLE_RIGHT_SIDE, 'Right Side'),
+        (ANGLE_LEFT_SIDE, 'Left Side'),
+        (ANGLE_BOTTOM, 'Bottom'),
+        (ANGLE_CUSTOM, 'Custom'),
+        (ANGLE_ALL, 'ALL'),
+    )
+
     created = models.DateTimeField(auto_now_add=True)
 
     sku_list = models.TextField()
+
+    image_type = models.CharField(
+        max_length=32,
+        default=TYPE_ALL,
+        choices=IMAGE_TYPE_CHOICES,
+    )
+
+    image_angle = models.CharField(
+        max_length=32,
+        default=ANGLE_ALL,
+        choices=IMAGE_ANGLE_CHOICES,
+    )
 
     image_format = models.CharField(
         max_length=16,
