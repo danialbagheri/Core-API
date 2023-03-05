2b2e8d59702d88f58c1d26016d6fc64a66a647cd
Ruhollah Sekaleshfar <r.sekaleshfar@fastbundle.co>
2023-03-06 02:34:17 +0330

    Filter images on image_type and image_angle on variant image requests

--- calypso/user/services/variant_image_zipper.py
+++ calypso/user/services/variant_image_zipper.py
@@ -2,13 +2,16 @@ import io
 import zipfile
 
 from product.models import ProductVariant
+from user.models import VariantImageRequest
 from .image_convertor import ImageConvertor
 
 
 class VariantImageZipper:
-    def __init__(self, sku_list, image_format):
+    def __init__(self, sku_list, variant_image_request):
         self.sku_list = sku_list
-        self.image_format = image_format
+        self.image_type = variant_image_request.image_type
+        self.image_angle = variant_image_request.image_angle
+        self.image_format = variant_image_request.image_format
         self.image_convertor = ImageConvertor(self.image_format)
         self.invalid_sku_list = []
         self.sku_list_without_images = []
@@ -19,7 +22,12 @@ class VariantImageZipper:
         if not variant:
             self.invalid_sku_list.append(sku)
             return
-        variant_image = variant.variant_images.first()
+        variant_image = variant.variant_images
+        if self.image_type != VariantImageRequest.TYPE_ALL:
+            variant_image = variant_image.filter(image_type=self.image_type)
+        if self.image_angle != VariantImageRequest.ANGLE_ALL:
+            variant_image = variant_image.filter(image_angle=self.image_angle)
+        variant_image = variant_image.first()
         if not variant_image:
             self.sku_list_without_images.append(sku)
             return
--- calypso/user/tasks/send_variant_images_email.py
+++ calypso/user/tasks/send_variant_images_email.py
@@ -12,7 +12,7 @@ class SendVariantImagesEmailTask(Task):
     def run(self, variant_image_request_id):
         variant_image_request = VariantImageRequest.objects.get(id=variant_image_request_id)
         sku_list = ast.literal_eval(variant_image_request.sku_list)
-        images_zipper = VariantImageZipper(sku_list, variant_image_request.image_format)
+        images_zipper = VariantImageZipper(sku_list, variant_image_request)
         zip_buffer = images_zipper.create_zip_file()
         images_email_service = VariantRequestEmailService(variant_image_request, zip_buffer)
         images_email_service.send_email()
