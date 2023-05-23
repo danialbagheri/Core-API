97d7312c5b996d69df2547a22e3ed8f3c47c0c53
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2023-05-23 17:40:09 +0330

    Alter image convertor to handle multiple formats

--- calypso/user/services/__init__.py
+++ calypso/user/services/__init__.py
@@ -5,3 +5,4 @@ from .review_reminder_sender import ReviewReminderSender
 from .add_subscriber import MarketingSubscriberService
 from .variant_image_zipper import VariantImageZipper
 from .variant_request_email import VariantRequestEmailService
+from .variant_images_retriever import VariantImagesRetriever
