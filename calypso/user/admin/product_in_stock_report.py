f477dd682045baf8b7a7b8dd050ec147bc873c4c
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2023-06-08 13:03:42 +0330

    Complete admin for SentEmail and ProductInStockReport models

--- calypso/user/admin/product_in_stock_report.py
+++ calypso/user/admin/product_in_stock_report.py
@@ -5,4 +5,18 @@ from user.models import ProductInStockReport
 
 @admin.register(ProductInStockReport)
 class ProductInStockReportAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        'id',
+        'variant_name',
+        'email',
+        'email_sent',
+    )
+    list_filter = ('email_sent',)
+    search_fields = ('email', 'variant__product__title')
+
+    def variant_name(self, product_in_stock_report: ProductInStockReport):
+        variant = product_in_stock_report.variant
+        product = variant.product
+        return f'{product.name} {variant.name}'
+
+    variant_name.short_description = 'Variant Name'
