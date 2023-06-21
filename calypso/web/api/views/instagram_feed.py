211426fa0a25f7ce18a20c1458ec0c452fe1dc0a
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2023-06-21 18:29:06 +0330

    Add instagram images path to settings

--- calypso/web/api/views/instagram_feed.py
+++ calypso/web/api/views/instagram_feed.py
@@ -22,15 +22,15 @@ class InstagramFeed(APIView):
         feed = []
         for data in queryset:
             single_post = {}
-            if data['media_type'] == "IMAGE" or data['media_type'] == "CAROUSEL_ALBUM":
+            if data['media_type'] == 'IMAGE' or data['media_type'] == 'CAROUSEL_ALBUM':
                 thumbnail, webp = self.reduce_photo_size(data['media_url'], data['id'])
-                single_post["thumbnail"] = thumbnail
-                single_post["webp"] = webp
-                single_post["caption"] = data["caption"]
-                single_post["permalink"] = data["permalink"]
-                single_post["id"] = data["id"]
-                single_post["media_url"] = data["media_url"]
-                single_post["media_type"] = data["media_type"]
+                single_post['thumbnail'] = thumbnail
+                single_post['webp'] = webp
+                single_post['caption'] = data['caption']
+                single_post['permalink'] = data['permalink']
+                single_post['id'] = data['id']
+                single_post['media_url'] = data['media_url']
+                single_post['media_type'] = data['media_type']
                 feed.append(single_post)
             else:
                 pass
@@ -40,12 +40,12 @@ class InstagramFeed(APIView):
     def reduce_photo_size(url, _id):
         current_site = Site.objects.get_current().domain
         media_root = str(settings.MEDIA_ROOT)
-        path = "/instagram/calypso/"
-        full_path = media_root + path + _id + ".jpg"
+        path = settings.INSTAGRAM_IMAGES_PATH
+        full_path = media_root + path + _id + '.jpg'
         if os.path.isfile(full_path) is not True:
             r = requests.get(url)
             pil_image = Image.open(BytesIO(r.content))
-            pil_image.save(f"{media_root}{path}{_id}.jpg")
-        image = current_site + get_thumbnail(full_path, '200x200', crop="center", quality=100, format="PNG").url
-        webp = current_site + get_thumbnail(full_path, '200x200', crop="center", quality=100, format="WEBP").url
+            pil_image.save(f'{media_root}{path}{_id}.jpg')
+        image = current_site + get_thumbnail(full_path, '200x200', crop='center', quality=100, format='PNG').url
+        webp = current_site + get_thumbnail(full_path, '200x200', crop='center', quality=100, format='WEBP').url
         return image, webp
--- calypso/web/utils/instagram.py
+++ calypso/web/utils/instagram.py
@@ -13,12 +13,12 @@ class InstagramUtils:
     def reduce_photo_size(url, _id):
         current_site = Site.objects.get_current().domain
         media_root = str(settings.MEDIA_ROOT)
-        path = "/instagram/calypso/"
-        full_path = media_root + path + _id + ".jpg"
+        path = settings.INSTAGRAM_IMAGES_PATH
+        full_path = media_root + path + _id + '.jpg'
         if os.path.isfile(full_path) is not True:
             r = requests.get(url)
             pil_image = Image.open(BytesIO(r.content))
-            pil_image.save(f"{media_root}{path}{_id}.jpg")
-        image = current_site + get_thumbnail(full_path, '200x200', crop="center", quality=100, format="PNG").url
-        webp = current_site + get_thumbnail(full_path, '200x200', crop="center", quality=100, format="WEBP").url
+            pil_image.save(f'{media_root}{path}{_id}.jpg')
+        image = current_site + get_thumbnail(full_path, '200x200', crop='center', quality=100, format='PNG').url
+        webp = current_site + get_thumbnail(full_path, '200x200', crop='center', quality=100, format='WEBP').url
         return image, webp
