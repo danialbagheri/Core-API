86ae95e629a07360613cfd9c7e6b129ee90cfb82
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2023-06-21 14:53:49 +0330

    Send base64 version of svg images in tag serializer

--- calypso/product/api/serializers/tag.py
+++ calypso/product/api/serializers/tag.py
@@ -1,9 +1,31 @@
+import base64
+
 from rest_framework import serializers
 
 from product.models import Tag
 
 
 class TagSerializer(serializers.ModelSerializer):
+    svg_icon = serializers.SerializerMethodField()
+
     class Meta:
         model = Tag
-        fields = '__all__'
+        fields = (
+            'id',
+            'icon',
+            'svg_icon',
+            'name',
+            'slug',
+        )
+
+    def get_svg_icon(self, tag: Tag):
+        svg_icon = tag.svg_icon
+        if not svg_icon:
+            return None
+
+        svg_path = svg_icon.path
+        with open(svg_path, 'rb') as svg_file:
+            svg_contents = svg_file.read()
+            encoded_svg = base64.b64encode(svg_contents).decode('utf-8')
+            data_uri = 'data:image/svg+xml;base64,' + encoded_svg
+        return data_uri
