e2e8e048d68db21d97133128bfb8b3b327187ef1
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2023-07-26 04:27:25 +0330

    Remove mysql settings from production.py

--- src/core/production.py
+++ src/core/production.py
@@ -12,16 +12,13 @@ CSRF_COOKIE_DOMAIN = env('CSRF_COOKIE_DOMAIN')
 SESSION_COOKIE_SAMESITE = None
 # Whether the session cookie should be secure (https:// only).
 SESSION_COOKIE_SECURE = False
-MYSQL_DB_NAME= env('MYSQL_DB_NAME')
-MYSQL_USER_NAME= env('MYSQL_USER_NAME')
-MYSQL_PASSWORD = env('MYSQL_PASSWORD')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 DATABASES = {
     'default': {
         'ENGINE': env('DB_ENGINE'),
-        'NAME': env('DB_NAME', default=env('MYSQL_DB_NAME')),
-        'USER': env('DB_USER', default=env('MYSQL_USER_NAME')),
-        'PASSWORD': env('DB_PASSWORD', default=env('MYSQL_PASSWORD')),
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
         'HOST': env('DB_HOST'),
         'PORT': env('DB_PORT'),
         'OPTIONS': env.json('DB_OPTIONS'),
