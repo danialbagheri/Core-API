5f6234d5281436b5aa26c7f4f1d30c60c618defe
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2023-08-31 15:17:23 +0330

    Add crispy bootstrap4

--- src/core/settings.py
+++ src/core/settings.py
@@ -86,6 +86,7 @@ INSTALLED_APPS = [
     'review',
     'faq',
     'surveys',
+
     'django.contrib.sitemaps',
     'rest_framework',
     'djoser',
@@ -98,6 +99,7 @@ INSTALLED_APPS = [
     'storages',
     'nested_admin',
     'django_cleanup.apps.CleanupConfig',
+    'crispy-bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -251,7 +253,10 @@ CORS_ALLOWED_ORIGINS = (
     'http://127.0.0.1:8000',
     'http://localhost:3000',
 )
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
 )
