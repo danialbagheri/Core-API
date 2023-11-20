cc14e85e0f849373cbbe927a42be4d1963c7bd04
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2023-11-20 18:47:22 +0330

    Fix image quality after extension conversion

--- src/user/services/image_convertor.py
+++ src/user/services/image_convertor.py
@@ -17,7 +17,7 @@ class ImageConvertor:
             image = Image.open(io.BytesIO(image_data))
             if image_format in ['JPG', 'JPEG']:
                 image = image.convert('RGB')
-            image.save(image_bytes, image_format if image_format != 'JPG' else 'JPEG')
+            image.save(image_bytes, image_format if image_format != 'JPG' else 'JPEG', quality=100)
             image_bytes.seek(0)
             images[image_format] = image_bytes
         return images
