ab163dc9b9f219710fb04f851ed9141ec34ead84
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2024-01-16 12:28:25 +0330

    Delete redundant files

--- src/common/permissions/event_bridge_permission.py
+++
@@ -1,8 +0,0 @@
-from django.conf import settings
-from rest_framework.permissions import BasePermission
-
-
-class EventBridgePermission(BasePermission):
-    def has_permission(self, request, view):
-        amazon_eventbridge_secret = request.headers.get('linco-eventbridge-secret')
-        return amazon_eventbridge_secret and amazon_eventbridge_secret == settings.AMAZON_EVENTBRIDGE_SECRET
